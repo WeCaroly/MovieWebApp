b97093b027a1927a66ce13577ff798a0441d46ef
Jordan Fay <jjfay7@gmail.com>
2017-11-27 15:52:59 -0600

    add

--- src/webapp/profile.java
+++ src/webapp/profile.java
@@ -1,14 +1,11 @@
 package webapp;
 
-import dataLayer.DBuser;
-import webapp.models.userModel;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
